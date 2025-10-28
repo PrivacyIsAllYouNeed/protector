fd8dea049da8b91c0959801e6e016d43d9e1b80a
tash-2s <81064017+tash-2s@users.noreply.github.com>
2025-10-28 09:52:10 -0700

    chore(client): use safe area context

--- mobile/client/app/index.tsx
+++ mobile/client/app/index.tsx
@@ -1,9 +1,13 @@
-import { SafeAreaView, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import VoiceChatScreen from "../src/screens/VoiceChatScreen";
 
 export default function Index() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#111" }}>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "#111" }}
+      edges={["top", "bottom"]}
+    >
       <StatusBar barStyle="light-content" />
       <VoiceChatScreen />
     </SafeAreaView>
