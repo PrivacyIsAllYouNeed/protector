0000a739326887f20383f17233051e2b1736f573
tash-2s <81064017+tash-2s@users.noreply.github.com>
2025-10-28 12:04:38 -0700

    add react compiler

--- mobile/client/eslint.config.js
+++ mobile/client/eslint.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
+const reactCompiler = require('eslint-plugin-react-compiler');
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
   expoConfig,
+  reactCompiler.configs.recommended,
   eslintPluginPrettierRecommended,
   {
     ignores: ["dist/*", ".expo/*"],
